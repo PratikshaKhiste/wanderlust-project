84a800808ba7dfc438d2629d12d0d28fb1f61ec2
Pratiksha Khiste <pratikshakhiste2006@gmail.com>
2026-03-20 12:00:36 +0530

    Fix render port configuration

--- app.js
+++ app.js
@@ -36,15 +36,17 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
+app.get("/",(req,res)=>{
+     res.redirect("/listings");
+});
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
-app.get("/",(req,res)=>{
-     res.redirect("/listings");
-});
+
 
 
 // const store=MongoStore.create({
