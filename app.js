b80b95924c65fa902be96c0a0403bfffbced7f92
Pratiksha Khiste <pratikshakhiste2006@gmail.com>
2026-03-20 11:15:36 +0530

    Add homepage redirect

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
-// app.get("/",(req,res)=>{
-//     res.send("Hi, I am root.");
-// });
+app.get("/",(req,res)=>{
+     res.redirect("/listings");
+});
 
 
 // const store=MongoStore.create({
